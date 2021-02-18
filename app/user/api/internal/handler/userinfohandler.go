aa41e09cb42888c40f980848730fbceb268169ed
Mikael <13247629622@163.com>
2021-02-18 10:32:17 +0700

    first

---
+++ app/user/api/internal/handler/userinfohandler.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"net/http"
+
+	"blog/app/user/api/internal/logic"
+	"blog/app/user/api/internal/svc"
+	"blog/app/user/api/internal/types"
+
+	"github.com/tal-tech/go-zero/rest/httpx"
+)
+
+func UserInfoHandler(ctx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.UserInfoRequest
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.Error(w, err)
+			return
+		}
+
+		l := logic.NewUserInfoLogic(r.Context(), ctx)
+		resp, err := l.UserInfo(req)
+		if err != nil {
+			httpx.Error(w, err)
+		} else {
+			httpx.OkJson(w, resp)
+		}
+	}
+}
